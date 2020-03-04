c4e7af4b9ee2085c9c77482e6eeabd871c39d1f1
Ilya <klimovia@altarix.ru>
2020-03-04 06:34:35 +0400

    Добавлена проверка ответов

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -12,18 +12,22 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     }
 
     fun listenAnswer(answer: String) =
-        if (question.answers.contains(answer)) {
-            question = question.nextQuestion()
-            "Отлично - ты справился\n${question.question}" to status.color
-        } else {
-            if (status != Status.CRITICAL) {
-                status = status.nextStatus()
-                "Это неправильный ответ\n${question.question}" to status.color
+        if (question.checkAnswer(answer)) {
+            if (question.answers.contains(answer)) {
+                question = question.nextQuestion()
+                "Отлично - ты справился\n${question.question}" to status.color
             } else {
-                status = Status.NORMAL
-                question = Question.NAME
-                "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+                if (status != Status.CRITICAL) {
+                    status = status.nextStatus()
+                    "Это неправильный ответ\n${question.question}" to status.color
+                } else {
+                    status = Status.NORMAL
+                    question = Question.NAME
+                    "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+                }
             }
+        } else {
+            "${question.error}\n${question.question}" to status.color
         }
 
     enum class Status(val color: Triple<Int, Int, Int>) {
@@ -43,24 +47,53 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
 
     enum class Question(val question: String, val answers: List<String>) {
         NAME("Как меня зовут?", listOf("бендер", "bender")){
+            override val error = "Имя должно начинаться с заглавной буквы"
+
             override fun nextQuestion() = PROFESSION
+
+            override fun checkAnswer(answer: String) = answer[0].isUpperCase()
         },
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
+            override val error = "Профессия должна начинаться со строчной буквы"
+
             override fun nextQuestion() = MATERIAL
+
+            override fun checkAnswer(answer: String) = answer[0].isLowerCase()
         },
         MATERIAL("Из чего я сделан?", listOf("метал", "дерево", "metal", "iron", "wood")) {
+            override val error = "Материал не должен содержать цифр"
+
             override fun nextQuestion() = BDAY
+
+            override fun checkAnswer(answer: String) = !answer.matches("-?\\d+(\\.\\d+)?".toRegex())
         },
         BDAY("Когда меня создали?", listOf("2993")) {
+            override val error = "Год моего рождения должен содержать только цифры"
+
             override fun nextQuestion() = SERIAL
+
+            override fun checkAnswer(answer: String) = answer.matches("-?\\d+(\\.\\d+)?".toRegex())
         },
         SERIAL("Мой серийный номер?", listOf("2716057")) {
+            override val error: String = "Серийный номер содержит только цифры, и их 7"
+
             override fun nextQuestion() = IDLE
+
+            override fun checkAnswer(answer: String) = answer.length == 7 && answer.matches("-?\\d+(\\.\\d+)?".toRegex())
         },
         IDLE("На этом все, вопросов больше нет", listOf()) {
+            override val error: String = ""
+
             override fun nextQuestion() = IDLE
+
+            override fun checkAnswer(answer: String) = true
         };
 
+        abstract val error: String
+
         abstract fun nextQuestion(): Question
+
+        abstract fun checkAnswer(answer: String): Boolean
+
     }
 }
